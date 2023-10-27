045e5a6701a9b8320616b8b610b469ac2335a723
4194304 <130800536+4194304@users.noreply.github.com>
2023-10-26 21:57:15 -0700

    debootstrapped!

---
+++ script.js
@@ -0,0 +1,14 @@
+function menuopen() {
+    document.getElementById('menu')
+    .style.display = "inline";
+    document.getElementById('navitemclose')
+    .style.display = "inline";
+    document.getElementById('menu')
+    .style.animationName = "menuopen";
+}  
+function menuclose() {
+    document.getElementById('menu')
+    .style.display = "none";
+    document.getElementById('navitemclose')
+    .style.display = "none";
+}  
