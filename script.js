84677ea9d1d342b1a53b932e476123d52aa2b42b
4194304 <130800536+4194304@users.noreply.github.com>
2023-10-28 09:05:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ function menuopen() {
     .style.display = "inline";
     document.getElementById('menu')
     .style.animationName = "menuopen";
+    document.getElementById('main')
+    .style.display = "none";
 }  
 function menuclose() {
     document.getElementById('menu')
     .style.display = "none";
     document.getElementById('navitemclose')
     .style.display = "none";
+    document.getElementById('main')
+    .style.display = "inline-block";
+    document.getElementById('main')
+    .style.maxWidth = "100%";
 }  
